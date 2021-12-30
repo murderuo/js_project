1b85888052a014536146e2a995c6314043603166
OkUgur <murderuo@users.noreply.github.com>
2021-12-30 23:35:28 +0300

    questions

---
+++ my_coderbyte/30-12-2021-coderbyte.js
@@ -0,0 +1,38 @@
+//ex.1
+
+/*
+
+AB Check
+Have the function ABCheck(str) take the str parameter being passed and return the string true if the characters a and b are separated by exactly 3 places anywhere in the string at least once (ie. "lane borrowed" would result in true because there is exactly three characters between a and b). Otherwise return the string false.
+Examples
+Input: "after badly"
+Output: false
+Input: "Laura sobs"
+Output: true
+
+
+*/
+
+#####################################################################
+//ex.2
+
+/*
+Distinct List
+Have the function DistinctList(arr) take the array of numbers stored in arr and determine the total number of duplicate entries. For example if the input is [1, 2, 2, 2, 3] then your program should output 2 because there are two duplicates of one of the elements.
+Examples
+Input: new int[] {0,-2,-2,5,5,5}
+Output: 3
+Input: new int[] {100,2,101,4}
+Output: 0
+*/
+########################################################################
+//ex.3
+/*
+Calculator
+Have the function Calculator(str) take the str parameter being passed and evaluate the mathematical expression within in. For example, if str were "2+(3-1)*3" the output should be 8. Another example: if str were "(2-0)(6/2)" the output should be 6. There can be parenthesis within the string so you must evaluate it properly according to the rules of arithmetic. The string will contain the operators: +, -, /, *, (, and ). If you have a string like this: #/#*# or #+#(#)/#, then evaluate from left to right. So divide then multiply, and for the second one multiply, divide, then add. The evaluations will be such that there will not be any decimal operations, so you do not need to account for rounding and whatnot.
+Examples
+Input: "6*(4/2)+3*1"
+Output: 15
+Input: "6/3-1"
+Output: 1
+*/
